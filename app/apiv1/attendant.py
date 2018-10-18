a5d390a52a70cdf6afd4a93e7f3e891cff95a4f6
Busobozihakiim <Busobozihakiim@yahoo.com>
2018-10-18 23:32:32 +0300

    create attendant endpoint which returns all products

---
+++ app/apiv1/attendant.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from tempdata.datamodal import *
+
+storeManager = Flask(__name__)
+
+@storeManager.route('/api/v1/products', methods=['GET'])
+def getAllProducts():
+    """Return all available products"""
+    if len(products) == 0:
+        return jsonify("No products available"),200
+    else:
+        return jsonify(products), 200
+
+if __name__ == "__main__":
+    storeManager.run(debug=True)
