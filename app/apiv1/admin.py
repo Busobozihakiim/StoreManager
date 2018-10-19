4a8a3fd1575b66dbb1fb9b1d2f076dfcb9c561e1
Busobozihakiim <Busobozihakiim@yahoo.com>
2018-10-19 18:46:22 +0300

    [finishes #161279539]fix to meet PEP standard

--- app/apiv1/admin.py
+++ app/apiv1/admin.py
@@ -1,15 +1,15 @@
 """this is for the admin"""
 from flask import Flask, jsonify
-from tempdata.datamodal import products
+from tempdata.datamodal import PRODUCTS
 
 STORE_MANAGER = Flask(__name__)
 
 @STORE_MANAGER.route('/api/v1/products', methods=['GET'])
 def get_all_products():
     """Return all available products"""
-    if not products:
+    if not PRODUCTS:
         return jsonify("No products available"), 200
-    return jsonify(products), 200
+    return jsonify(PRODUCTS), 200
 
 if __name__ == "__main__":
     STORE_MANAGER.run(debug=True)
