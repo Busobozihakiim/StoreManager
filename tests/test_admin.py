52c134cd26413edd519a4c52d332b23fa3460de2
Busobozihakiim <Busobozihakiim@yahoo.com>
2018-10-19 20:58:11 +0300

    [finishes #161279587] create unit tests for admin and attendant

---
+++ tests/test_admin.py
@@ -0,0 +1,14 @@
+"""UNIT TESTING"""
+import unittest
+import requests
+
+class Testadmin(unittest.TestCase):
+    """Class for unit tests"""
+
+    def test_get_single_product(self):
+        """test the return of a single product based on id"""
+        self.response = requests.get('http://127.0.0.1:5000/api/v1/products/{}'.format(2))
+        self.assertEqual(self.response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
