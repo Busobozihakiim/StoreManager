9c287bf2190e6fcb51f3e0373ed34347be9b29e3
Busobozihakiim <Busobozihakiim@yahoo.com>
2018-10-19 17:51:07 +0300

    [delivers #161279539]change endpoint to meet PEP 8 standard

--- app/apiv1/admin.py
+++ app/apiv1/admin.py
@@ -1,16 +1,15 @@
-#this is for the admin
-from flask import Flask, request, jsonify
-from tempdata.datamodal import *
+"""this is for the admin"""
+from flask import Flask, jsonify
+from tempdata.datamodal import products
 
-storeManager = Flask(__name__)
+STORE_MANAGER = Flask(__name__)
 
-@storeManager.route('/api/v1/products', methods=['GET'])
-def getAllProducts():
+@STORE_MANAGER.route('/api/v1/products', methods=['GET'])
+def get_all_products():
     """Return all available products"""
-    if len(products) == 0:
-        return jsonify("No products available"),200
-    else:
-        return jsonify(products), 200
+    if not products:
+        return jsonify("No products available"), 200
+    return jsonify(products), 200
 
 if __name__ == "__main__":
-    storeManager.run(debug=True)
+    STORE_MANAGER.run(debug=True)
