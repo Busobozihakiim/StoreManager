01137c9957992a72fcc5213f0b406d2b0c6f649f
Busobozihakiim <Busobozihakiim@yahoo.com>
2018-10-20 17:43:49 +0300

    [Finishes #161331973] create unit test for getting a single sale record enpoint

---
+++ tests/test_admin.py
@@ -0,0 +1,14 @@
+"""UNIT TESTING"""
+import unittest
+import requests
+
+class Testadmin(unittest.TestCase):
+    """Class for unit tests"""
+
+    def test_get_single_sale(self):
+        """test the return of a single sale based on id"""
+        self.response = requests.get('http://127.0.0.1:5000/api/v1/sales/{}'.format(2))
+        self.assertEqual(self.response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
